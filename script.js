bbdf0ac346a2b4ce6b51451dba99fcdf49f86cc4
muivproject <m70155302@yandex.ru>
2022-04-12 17:45:42 +0300

    done

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const w = canvas.width = innerHeight;
-const h = canvas.height = innerHeight;
+const w = canvas.width = innerHeight - 50;
+const h = canvas.height = innerHeight - 50;
 
 const imgData = ctx.createImageData(w, h);
 const data = imgData.data;
+
+const max_iter = 100;
+const minVal = -2.5;
+const maxVal = 2.5;
 for (let x = 0; x < w; x++) {
     for (let y = 0; y < w; y++) {
-        let a = normalize(x, [0, w], [-2, 2]);
-        let b = normalize(y, [0, h], [-2, 2]);
+        let a = normalize(x, 0, w, minVal, maxVal);
+        let b = normalize(y, 0, h, minVal, maxVal);
 
-        let z = 0;
+        let ca = a;
+        let cb = b;
+
         let n = 0;
 
-        for (n; n < 100; n++) {
-            let aa = a ** 2 - b ** 2;
-            let bb = 2 * a * b;
+        for (n; n < max_iter; n++) {
+            let z = a ** 2 - b ** 2;
+            let c = 2 * a * b;
 
-            a = aa;
-            b = bb;
+            a = z + ca;
+            b = c + cb;
 
-            if (Math.abs(a + b) > 16) break;
+            if (Math.abs(a + b) > 20) break;
         }
 
-        let bright = 0;
-        if (n == 100) {
-            bright = 255;
+        let bright = normalize(n, 0, max_iter, 0, 1);
+        bright = normalize(Math.sqrt(bright), 0, 1, 0, 255);
+        if (n == max_iter) {
+            bright = 0;
         }
 
         let pix = (x + y * w) * 4;
@@ -37,11 +44,11 @@ for (let x = 0; x < w; x++) {
     }
 }
 
-function normalize(val, currentRange, targetRange) {
-    const currStart = currentRange[0]
-    const currEnd = currentRange[1];
-    const tarStart = targetRange[0];
-    const tarEnd = targetRange[1];
+function normalize(val, ...ranges) {
+    const currStart     = ranges[0];
+    const currEnd       = ranges[1];
+    const tarStart      = ranges[2];
+    const tarEnd        = ranges[3];
 
     const currLength = currEnd - currStart;
     const tarLength = tarEnd - tarStart;
@@ -53,4 +60,3 @@ function normalize(val, currentRange, targetRange) {
 }
 
 ctx.putImageData(imgData, 0, 0);
-
